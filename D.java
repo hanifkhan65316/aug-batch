e7fd997b3d399d995856e219a91aad29d24f34a8
hanifkhan65316 <113525035+hanifkhan65316@users.noreply.github.com>
2022-09-14 18:42:38 +0530

    my first uploaded file
    my first file

---
+++ D.java
@@ -0,0 +1,14 @@
+class  D
+{
+	public static void main(String[] args) 
+	{
+
+		System.out.println("main begin");
+		for(int i = 40; i <=30; i += 4)  // i = i+4
+		{
+		System.out.println("loop body" +i);
+		}
+		System.out.println("main end");
+	}
+}
+
